8d5b15374469b2bdf0f0f24f622aa4fd798533b4
Maciek Jurczyk <thelairmail@gmail.com>
2015-12-24 13:31:30 +0100

    restructure-gulp: tmp dir for CSS - moved temporary CSS files to systems TMP dir - commented out unused code in gulpfile - bumped gulp version in package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,38 @@
 /** @jsx React.DOM */
-var browserify = require('gulp-browserify');
-var concat = require('gulp-concat');
-var connect = require('gulp-connect');
 var gulp = require('gulp');
-var open = require('gulp-open');
+//var browserify = require('gulp-browserify'); // NOTE Unused
+//var concat = require('gulp-concat'); // NOTE Unused
+//var connect = require('gulp-connect'); // NOTE Unused
+//var open = require('gulp-open'); // NOTE Unused
 var less = require('gulp-less');
 var path = require('path');
 var uglify = require('gulp-uglify');
 var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
+var os = require('os');
 
-gulp.task('copy', function () {
-  gulp.src('src/*')
-    .pipe(gulp.dest('dist'));
-});
+// NOTE Unused
+//gulp.task('copy', function () {
+//  gulp.src('src/*')
+//    .pipe(gulp.dest('dist'));
+//});
+
+var tmpDirCss = path.join(os.tmpdir(), 'css');
 
 gulp.task('less', function () {
+  var tmpdir = os.tmpdir();
+  
   gulp.src('src/less/*.less')
     .pipe(less({
       paths: [
         path.join(__dirname, 'less', 'includes')
       ]
     }))
-    .pipe(gulp.dest('src/css'));
+    .pipe(gulp.dest(tmpDirCss));
 });
 
-gulp.task('styles', ['less'] function () {
-  gulp.src('src/css/*.css')
+gulp.task('styles', ['less'], function () {
+  gulp.src(path.join(tmpDirCss, '/*.css'))
     .pipe(cssmin())
     .pipe(rename({
       suffix: '.min'
@@ -47,4 +53,4 @@ gulp.task('watch', function () {
   gulp.watch('src/**/*', ['default']);
 });
 
-gulp.task('default', ['css', 'minify']);
+gulp.task('default', ['styles', 'minify']);
