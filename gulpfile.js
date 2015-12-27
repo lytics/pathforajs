e30c9412e68f05b0ec87765382fcbbdc2f7bcdd6
Maciek Jurczyk <thelairmail@gmail.com>
2015-12-27 22:51:21 +0100

    gulp-preview: added gulp webserver for preview

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var path = require('path');
 var uglify = require('gulp-uglify');
 var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
+var connect = require('gulp-connect');
 
 gulp.task('build:styles', function () {
   gulp.src('src/less/*.less')
@@ -16,7 +17,8 @@ gulp.task('build:styles', function () {
     .pipe(rename({
       suffix: '.min'
     }))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist'))
+    .pipe(connect.reload());
 });
 
 gulp.task('build:js', function () {
@@ -25,11 +27,20 @@ gulp.task('build:js', function () {
     .pipe(rename({
       suffix: '.min'
     }))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist'))
+    .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
   gulp.watch('src/**/*', ['default']);
 });
 
-gulp.task('default', ['build:styles', 'build:js']);
+gulp.task('preview', function () {
+  connect.server({
+    port: 8080,
+    root: '.',
+    livereload: true
+  });
+});
+
+gulp.task('default', ['build:styles', 'build:js', 'preview']);
