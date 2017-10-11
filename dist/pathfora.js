4867b5a188f3de1d6fcee2443724191596859cc0
Sarah Yablok <contact@sarahyablok.com>
2017-10-11 11:44:50 -0700

    check that callback is a function.

--- dist/pathfora.js
+++ dist/pathfora.js
@@ -3095,6 +3095,12 @@ function recommendContent (accountId, params, id, callback) {
   // Recommendation API:
   // https://www.getlytics.com/developers/rest-api#content-recommendation
 
+  // ensure that the callback arg is a function
+  if (typeof callback !== 'function') {
+    console.warn('Could not make recommendation - missing callback function.');
+    return;
+  }
+
   // if we have the recommendation response cached in session storage
   // use that instead of making a new API request
   var storedRec = sessionStorage.getItem(PREFIX_REC + id);
