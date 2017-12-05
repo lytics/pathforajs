493cca2c65930414e98af9378f4432528a521d10
Sarah Yablok <contact@sarahyablok.com>
2017-12-05 13:50:37 -0800

    version 0.2.14

--- dist/pathfora.js
+++ dist/pathfora.js
@@ -218,7 +218,7 @@ function createABTestingModePreset () {
 
 // globals
 // ab tests
-var PF_VERSION = '0.2.13';
+var PF_VERSION = '0.2.14';
 var PF_LOCALE = 'en-US';
 var PF_DATE_OPTIONS = {};
 var PREFIX_REC = 'PathforaRecommend_';
