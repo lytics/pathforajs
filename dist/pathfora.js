9c1cb8b370f872e2e57d24178535a61b5a9b7eb4
Sarah Yablok <contact@sarahyablok.com>
2017-12-05 13:48:08 -0800

    build prod.

--- dist/pathfora.js
+++ dist/pathfora.js
@@ -833,15 +833,11 @@ function getObjectValue (object, key) {
  * @returns {string} id
  */
 function generateUniqueId () {
-  var s4;
-
-  if (typeof s4 === 'undefined') {
-    s4 = function () {
-      return Math.floor((1 + Math.random()) * 0x10000)
-        .toString(16)
-        .substring(1);
-    };
-  }
+  var s4 = function () {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  };
 
   return [
     s4(), s4(),
