cb2762f1f6edbfc9aed02c15d780545ec187023c
Sarah <me@sarahyablok.com>
2019-01-11 13:44:13 -0800

    version 1.1.2

--- dist/pathfora.js
+++ dist/pathfora.js
@@ -213,7 +213,7 @@
 
   /** @module pathfora/globals/config */
 
-  var PF_VERSION = '1.1.1',
+  var PF_VERSION = '1.1.2',
       PF_LOCALE = 'en-US',
       PF_DATE_OPTIONS = {},
       PREFIX_REC = 'PathforaRecommend_',
