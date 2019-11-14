4a40653534cf486529602fc9522ec7d98c37cb0a
markhayden <hi@markhayden.me>
2019-11-13 16:50:34 -0800

    we actually want to do a minor release so 1.2.0 it is

--- dist/pathfora.js
+++ dist/pathfora.js
@@ -213,7 +213,7 @@
 
   /** @module pathfora/globals/config */
 
-  var PF_VERSION = '1.1.7',
+  var PF_VERSION = '1.2.0',
       PF_LOCALE = 'en-US',
       PF_DATE_OPTIONS = {},
       PREFIX_REC = 'PathforaRecommend_',
