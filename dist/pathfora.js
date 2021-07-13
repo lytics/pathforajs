b9620dc2d4a47baf4f50d5812813683bbd02ab4c
Sarah Yablok <me@sarahyablok.com>
2021-07-13 10:26:51 -0700

    version 1.2.7

--- dist/pathfora.js
+++ dist/pathfora.js
@@ -213,7 +213,7 @@
 
   /** @module pathfora/globals/config */
 
-  var PF_VERSION = '1.2.6',
+  var PF_VERSION = '1.2.7',
       PF_LOCALE = 'en-US',
       PF_DATE_OPTIONS = {},
       PREFIX_REC = 'PathforaRecommend_',
